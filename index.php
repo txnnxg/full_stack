95d50248cebf92fb6583d6e24163de858163931a
txnnxg <143152390+txnnxg@users.noreply.github.com>
2025-02-26 00:03:27 -0300

    excluida tela de login e registro

--- index.php
+++ index.php
@@ -12,16 +12,15 @@
     <header class="header">
         <nav class="navbar">
             <a href="#home"><i class="fas fa-home"></i><span>Home</span></a>
+            < !-- "http://localhost/full_stack/tela-de-login.php"(Link original da tela de login) -- >
             <a href="https://fullstack-production-80ef.up.railway.app/tela-de-login.php"><i class="fas fa-user"></i><span>login</span></a>
+            < !-- "http://localhost/full_stack/formulario.php"(Link original da tela de registro) -- >
             <a href="https://fullstack-production-80ef.up.railway.app/formulario.php"><i class="fas fa-contact-card"></i><span>Register</span></a>
         </nav>
     </header>
 
     <div class="container">
         <section id="home" class="home">academia popular fit</section>
-        < !-- "http://localhost/full_stack/tela-de-login.php"(Link original da tela de login) -- >
-        <section id="about" class="about"><a href="tela-de-login.php" style="text-decoration: none; color:aliceblue">Login</a></section>
-        < !-- "http://localhost/full_stack/formulario.php"(Link original da tela de registro) -- >
     </div>
 </body>
 </html>
