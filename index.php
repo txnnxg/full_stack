ba0f253a32c9472dd7f75b481ad21c9096abb053
txnnxg <143152390+txnnxg@users.noreply.github.com>
2025-02-25 23:42:30 -0300

    Testando link para voltar

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <div class="container">
         <section id="home" class="home">academia popular fit</section>
         < !-- "http://localhost/full_stack/tela-de-login.php"(Link original da tela de login) -- >
-        <section id="about" class="about"><a href="https://fullstack-production-80ef.up.railway.app/tela-de-login.php" style="text-decoration: none; color:aliceblue">Login</a></section>
+        <section id="about" class="about"><a href="tela-de-login.php" style="text-decoration: none; color:aliceblue">Login</a></section>
         <section id="contact" class="contact"><a href="http://localhost/full_stack/formulario.php" style="text-decoration: none; color:aliceblue">Register</a></section>
     </div>
 </body>
