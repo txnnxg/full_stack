865d2ac27f3da5912e022c0998aa2775f04cce22
txnnxg <143152390+txnnxg@users.noreply.github.com>
2025-02-25 23:56:40 -0300

    Testando tela de registro

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <nav class="navbar">
             <a href="#home"><i class="fas fa-home"></i><span>Home</span></a>
             <a href="https://fullstack-production-80ef.up.railway.app/tela-de-login.php"><i class="fas fa-user"></i><span>login</span></a>
-            <a href="#contact"><i class="fas fa-contact-card"></i><span>Register</span></a>
+            <a href="https://fullstack-production-80ef.up.railway.app/formulario.php"><i class="fas fa-contact-card"></i><span>Register</span></a>
         </nav>
     </header>
 
@@ -21,7 +21,7 @@
         <section id="home" class="home">academia popular fit</section>
         < !-- "http://localhost/full_stack/tela-de-login.php"(Link original da tela de login) -- >
         <section id="about" class="about"><a href="tela-de-login.php" style="text-decoration: none; color:aliceblue">Login</a></section>
-        <section id="contact" class="contact"><a href="http://localhost/full_stack/formulario.php" style="text-decoration: none; color:aliceblue">Register</a></section>
+        < !-- "http://localhost/full_stack/formulario.php"(Link original da tela de registro) -- >
     </div>
 </body>
 </html>
